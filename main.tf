334277e342aa9c52a46ab2a9a629e9738d242bd9
Talib Idris <olarinre@gmail.com>
2018-08-01 15:01:01 +0100

    added comments for resources

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
+# The combination of these resources will create an aws vpc with access to the internet, internal routing and a single public subnet
+
+# defines vpc resource
 resource "aws_vpc" "tfb" {
-  cidr_block = "${var.cidr}"
+  cidr_block           = "${var.cidr}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
-  enable_dns_support = "${var.enable_dns_support}"
+  enable_dns_support   = "${var.enable_dns_support}"
   tags {
       Name = "${var.name}"
   }
 }
 
+# defines internet gateway resource
 resource "aws_internet_gateway" "tfb" {
   vpc_id = "${aws_vpc.tfb.id}"
   tags {
@@ -14,14 +18,16 @@ resource "aws_internet_gateway" "tfb" {
   }
 }
 
+# defines internet access route resource
 resource "aws_route" "internet_access" {
-  route_table_id = "${aws_vpc.tfb.main_route_table_id}"
+  route_table_id         = "${aws_vpc.tfb.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.tfb.id}"
+  gateway_id             = "${aws_internet_gateway.tfb.id}"
 }
 
+# defines public subnet resource
 resource "aws_subnet" "public" {
-  vpc_id = "${aws_vpc.tfb.id}"
+  vpc_id     = "${aws_vpc.tfb.id}"
   cidr_block = "${var.public_subnet}"
   tags {
       Name = "${var.name}-public"
